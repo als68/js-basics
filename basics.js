24e9ad13e71923385dd74b0ac4d1d468cb229e8e
root <suhandyl@gmail.com>
2016-06-06 18:26:09 -1000

    Finished person, canDrive, and greet tests

--- basics.js
+++ basics.js
@@ -1,24 +1,47 @@
 /* Create a `myName` variable and assign it a String value */
+var myName = "Andy";
 
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
  */
 
+ var person = {
+ 	name: "Andy",
+ 	age: 33
+ }
+
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
+
+ var canDrive;
+ if (person.age > 15) {
+ 	canDrive = true;
+ }
 
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
 
+var greet = function(name) {
+	console.log("Hello, my name is {name}");
+} 
+
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
  */
+
+ var dataTypes = ["Variable 1", 73, false, undefined, [1, 2, 3], {testName: 4, testName2: 5, testName3: 6}];
 
 /* Create a `dog` object
  * it should have a `bark` function that makes your dog bark!
  * It should also have a name attribute with the value of 'Spot'
  */
+var dog = {
+	bark: function() {
+		console.log("woof");
+	},
+	name: "Spot"
+}
